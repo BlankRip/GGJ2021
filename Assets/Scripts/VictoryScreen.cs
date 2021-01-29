f9a0c64369b3d17c95ee8a516f21c35f348606b1
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2021-01-29 15:54:06 +0400

    fades and win

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VictoryScreen : MonoBehaviour
+{
+    [SerializeField] GameObject fadeOutPanel;
+    [SerializeField] VideoClip creditsClip;
+
+    private void Start() {
+        StartCoroutine(TriggerOnVideoEnd());
+    }
+
+    private IEnumerator TriggerOnVideoEnd() {
+        yield return new WaitForSeconds((float)creditsClip.length);
+        //Setting game progress to be game completed
+        if(ProgressTracker.instance != null)
+            ProgressTracker.instance.GameCompleted();
+
+        fadeOutPanel.SetActive(true);
+    }
+}
