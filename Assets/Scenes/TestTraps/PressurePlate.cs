13299608ba39e115ebc512bcca7564d650bc93f6
turmoil2k <juma1112009@hotmail.com>
2021-01-29 23:45:34 +0400

    Pushing Pressure Plate Puzzle with nice features

---
+++ Assets/Scenes/TestTraps/PressurePlate.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public bool pressed;
+    public float pressedTimer; // def val 5
+    Vector3 pressedPos;
+    Vector3 mainPos;
+    float timer;
+    public GameObject pressurePlateChild;
+    public GameObject sphereIndicator;
+    Renderer sphereRenderer;
+    private void Start()
+    {
+        pressed = false;
+        pressedPos = new Vector3(transform.position.x, transform.position.y - 0.15f, transform.position.z);
+        mainPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        sphereRenderer = sphereIndicator.GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        if (pressed == true)
+        {
+            pressurePlateChild.transform.position = Vector3.Lerp(pressurePlateChild.transform.position, pressedPos, Time.deltaTime * 2);
+            timer += Time.deltaTime;
+            if (timer >= pressedTimer)
+            {
+                pressed = false;
+                timer = 0;
+            }
+            sphereRenderer.material.color = Color.green;
+        }
+        else
+        {
+            pressurePlateChild.transform.position = Vector3.Lerp(pressurePlateChild.transform.position, mainPos, Time.deltaTime * 2);
+            sphereRenderer.material.color = Color.red;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pressed = true;
+        }
+    }
+}
