c7090c037f1479e6f899a823e123794dbb8829da
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2021-01-29 13:35:05 +0400

    Perks work

--- Assets/Scripts/Perks.cs
+++ Assets/Scripts/Perks.cs
@@ -33,19 +33,68 @@ public class Perks : MonoBehaviour
     private float initialSpeed;
     private float initialJump;
     private float initialDecay;
+    
+    float timer = 0;
+    [SerializeField] float timeToSwitch;
+
+    List<string> inactivePerks = new List<string>{ "Speed", "Jump", "Dash", "Decay", "DoubleJump"};
+    List<string> activePerks = new List<string>();
 
     private void Start() {
         theScript = GameManager.instance.playerScript;
         initialSpeed = theScript.speed;
         initialJump = theScript.jumpeForce;
         initialDecay = theScript.healthDcayRate;
+        for (int i = 0; i < 3; i++)
+        {
+            PickPerk();
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeToSwitch)
+        {
+            Switch();
+            timer = 0;
+        }
+    }
+
+    private void Switch()
+    {
+        int rand = Random.Range(0, activePerks.Count);
+        string toRemove = activePerks[rand];
+        PickPerk();
+        activePerks.Remove(toRemove);
+        inactivePerks.Add(toRemove);
     }
 
     private void PickPerk() {
-        
+        int rand = Random.Range(0, inactivePerks.Count);
+        switch (inactivePerks[rand])
+        {
+            case "Speed": 
+                SpeedPerks();
+                break;
+            case "Jump":
+                JumpPerks();
+                break;
+            case "DoubleJump":
+                DoubleJump();
+                break;
+            case "Dash":
+                DashingMan();
+                break;
+            case "Decay":
+                DecayPerks();
+                break;
+        }
     }
 
     private void SpeedPerks() {
+        inactivePerks.Remove("Speed");
+        activePerks.Add("Speed");
         float picker = Random.Range(0.5f, 2.5f);
         if(picker > 1.2f) {
             //then increase perk
@@ -58,6 +107,8 @@ public class Perks : MonoBehaviour
     }
 
     private void JumpPerks() {
+        inactivePerks.Remove("Speed");
+        activePerks.Add("Speed");
         float picker = Random.Range(0.5f, 2.5f);
         if(picker > 1.4f) {
             //then increase perk
@@ -73,6 +124,8 @@ public class Perks : MonoBehaviour
     }
 
     private void DecayPerks() {
+        inactivePerks.Remove("Decay");
+        activePerks.Add("Decay");
         float picker = Random.Range(0.5f, 3.4f);
         if(picker > 0.4f && picker < 1.75f) {
             //then increase perk
@@ -88,10 +141,14 @@ public class Perks : MonoBehaviour
     }
 
     private void DoubleJump() {
+        inactivePerks.Remove("DoubleJump");
+        activePerks.Add("DoubleJump");
         theScript.jumpenni = true;
     }
 
     private void DashingMan() {
+        inactivePerks.Remove("Dash");
+        activePerks.Add("Dash");
         theScript.dasher = true;
     }
 
