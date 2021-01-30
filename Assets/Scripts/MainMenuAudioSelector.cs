815db45086324a52d934ba88adc9395bb987d9b0
Vishnu Raveendran <vishnuraveendran246@gmail.com>
2021-01-30 15:58:08 +0400

    added credits scene

---
+++ Assets/Scripts/MainMenuAudioSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAudioSelector : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> audioClips;
+    [SerializeField] AudioSource menuBGM;
+
+    private void Start()
+    {
+        menuBGM.clip = SelectRandom();
+        menuBGM.Play();
+    }
+
+    public AudioClip SelectRandom()
+    {
+        return audioClips[Random.Range(0,audioClips.Count)];
+    }
+}
